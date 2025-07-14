ee4ac2cf3c6016f5638755e7e1582f33d2ffd10b
Abdullah-SE26 <m.abdullahx21@gmail.com>
2025-07-14 15:56:15 +0400

    fixed some bugs regarding the startup card

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,10 +1,10 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current)] | order(createdAt desc){
-  id,
+  _id,
   title,
   slug,
-  createdAt,
+  _createdAt,
   author->{
     authorId,
     name,
