46d18fd3e0eeb52960eff19d78d08ca3d3a8feec
Abdullah-SE26 <m.abdullahx21@gmail.com>
2025-07-15 15:44:37 +0400

    implementation of user authentication flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,10 +3,13 @@ import HeadingBox from "../../components/HeadingsBox";
 import SearchBar from "../../components/SearchBar";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 const Home = async ({ searchParams }: { searchParams: { query?: string } }) => {
   const query = (await searchParams).query;
   const params = {search: query || null}
+
+  const session = await auth()
   const {data : posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
 
