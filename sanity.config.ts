21172584462ef8ee214b93ba90aece9c2535abb8
Abdullah-SE26 <m.abdullahx21@gmail.com>
2025-07-17 16:34:04 +0400

    fix: sanity config issues

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,11 +6,10 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import { markdownSchema } from 'sanity-plugin-markdown';
+import {deskTool} from 'sanity/desk' // <--- correct import
 
+import {markdownSchema} from 'sanity-plugin-markdown'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
@@ -19,13 +18,10 @@ export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
+    deskTool({structure}), // use deskTool instead of structureTool
     visionTool({defaultApiVersion: apiVersion}),
-    markdownSchema()
+    markdownSchema(),
   ],
 })
